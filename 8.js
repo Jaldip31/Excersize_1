e761e06c4d747b3c445b4fd4bda2e7acbefedd77
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-02-11 18:21:42 +0530

    11-02-2022

--- 8.js
+++ 8.js
@@ -1,38 +1,43 @@
 //8. Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
 
 //anonymous function
-var prime = function (num) {
+var prime1 = function (num) {
     let count = 0
     for (i = 2; i < num; ++i)
         (num % i == 0) ? count++ : 0;
-        
-    return (count == 0) ? "prime": "not prime";
+    return (count == 0) ? "prime" : "not prime";
 }
-console.log(prime(7));
+console.log(prime1(7));                                                                     //prime
 
-// // arrow function
-// var prime = (num) => {
-//     var count = 0
-//     for (i = 2; i < num; i++) {
-//         if (num % i == 0) {
-//             count++;
-//         }
-//     }
-//     return (count == 0) ? console.log("prime") : console.log("not prime");
-// }
-// prime(10)
+// arrow function
+var prime2 = (num) => {
+    let count = 0
+    for (i = 2; i < num; i++)
+        (num % i == 0) ? count++ : 0;
+    return (count == 0) ? "prime" : "not prime";
+}
+console.log(prime2(10));                                                                    //not prime
 
-// //call back function by anoymous
-// function prime1(abc) {
-//     console.log("ans : ", abc)
-//     abc(6)
-// }
-// prime1(function (number) {
-//     var c = 0
-//     for (i = 2; i < number; i++) {
-//         if (number % i == 0) {
-//             c++
-//         }
-//     }
-//     (c == 0) ? console.log("prime") : console.log("not prime")
-// })
+//call back function by anoymous
+function prime3(abc) {
+    console.log("ans : ", abc)                                                              //ans :  [Function (anonymous)]
+    console.log(abc(6))                                                                     //not prime
+}
+prime3(function (num) {
+    let count = 0;
+    for (i = 2; i < num; i++)
+        (num % i == 0) ? count++ : 0;
+    return (count == 0) ? "prime" : "not prime";
+})
+
+//call back function by arrow 
+function prime4(abc) {
+    console.log("ans : ", abc)                                                              //ans :  [Function (anonymous)]
+    console.log(abc(13))                                                                    //prime
+}
+prime4((num) => {
+    let count = 0;
+    for (i = 2; i < num; i++)
+        (num % i == 0) ? count++ : 0;
+    return (count == 0) ? "prime" : "not prime";
+})
