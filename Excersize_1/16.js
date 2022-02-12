97a5c896de77703905c77fb828692dac3a015844
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-02-12 19:40:59 +0530

    12-02-2022

--- Excersize_1/16.js
+++ Excersize_1/16.js
@@ -1,6 +1,30 @@
-var repeat=function(str,q){
-    var a=""
-    console.log(str.replace(q,a))
+//16. Write a JavaScript function to extract unique characters from a string.  Example string : "thequickbrownfoxjumpsoverthelazydog" Expected Output : "thequickbrownfxjmpsvlazydg
+
+var repeat1=function(str,q){
+    return str.replaceAll(q,"");
 }
- 
- console.log(repeat("thequickbrownfoxjumpsoverthelazydog","the"))
+ console.log(repeat1("thequickbrownfoxjumpsoverthelazydog","e"));                                        //thquickbrownfoxjumpsovrthlazydog
+
+ var repeat2=(str,q)=>{
+    return str.replaceAll(q,"");
+}
+ console.log(repeat2("Java Script","a"));                                                                //Jv Script
+
+
+ //call back function by anonymous
+function repeat3(abc) {
+    console.log("ans : ", abc);                                                                          //ans :  [Function (anonymous)]
+    console.log(abc("Skill Qode","l"));                                                                  //Ski Qode                            
+}
+repeat3 (function(str,q){
+    return str.replaceAll(q,"");
+})
+
+ //call back function by arrow
+ function repeat4(abc) {
+    console.log("ans : ", abc);                                                                          //ans :  [Function (anonymous)]
+    console.log(abc("Hello world","o"));                                                                 //Hell wrld                           
+}
+repeat4 ((str,q)=>{
+    return str.replaceAll(q,"");
+})
--- Excersize_1/23.js
+++ Excersize_1/23.js
@@ -1,60 +1,107 @@
-var repeat=function(n){
-   arr=n.split("")
-   console.log(arr)
-   n.filter(arr)
+// //23. Write a JavaScript function to find the first not repeated character.  Sample arguments : 'abacddbec' Expected output : 'e' 
+// var repeat=function(str){
+   
 
-}
+//    for(i=0;i<str.length;i++){
+//       x=str[0]
+//       str=str.replace(str[0],"")
 
+//       if(str.search(x)==-1){}
 
-console.log(repeat("abacdedbc",))
+//       str=str.replaceAll(x,"")
+//       console.log(str)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// n="abacdedbc"
-// var repeat=function(n){
-//     n=n.split("")
-//     for(i=0;i<n.length;i++)
-//     {
-//         var count=0
-//         for(j=0;j<n.length;j++)
-//         {
-//             if(n[i]==n[j])
-//             {
-//                 count++
-//                 // console.log("hi")
-//             }
-//         }
-
-//         if(count<2)
-//         {
-//             result = n[i];
-//             break;
-//         }
-//     }
-//         console.log(result);
 //    }
-// repeat("abacddbec")
+// }
+
+// repeat("abacdedbc");
+
+// // str="abacdedbc"
+// // i=3
+// // if(str.includes(str[i])){
+
+// //    console.log("hello");
+
+// // }
+// // console.log(true==false)
+
+
+
+
+
+
+
+// // b=str.shift();
+// //       console.log(b)
+// //       console.log(str)
+// //       console.log(str.includes(b))
+// //       str.shift()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // n="abacdedbc"
+// // var repeat=function(n){
+// //     n=n.split("")
+// //     for(i=0;i<n.length;i++)
+// //     {
+// //         var count=0
+// //         for(j=0;j<n.length;j++)
+// //         {
+// //             if(n[i]==n[j])
+// //             {
+// //                 count++
+// //                 // console.log("hi")
+// //             }
+// //         }
+
+// //         if(count<2)
+// //         {
+// //             result = n[i];
+// //             break;
+// //         }
+// //     }
+// //         console.log(result);
+// //    }
+// // repeat("abacddbec")
+
+function find_firstNotRepeatedChar(str) {
+   let count = 0;
+   let str2 = "";
+   for (let i = 0; i < str.length; i++) {
+      // console.log(str)
+      let index = str.indexOf(str[i], 0);
+      console.log(index)
+       while (index != -1) {
+           count++;
+           index = str.indexOf(str[i], index + 1);  
+       }
+       if(count === 1) {
+          str2 += str[i];
+          console.log(str2)
+       }
+       count=0;
+       
+   }
+   return str2[0];
+}
+console.log(find_firstNotRepeatedChar('abacddbec'));
+
+// console.log(0 != -1)
